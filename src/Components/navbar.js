aac89bb945487770935fc6675d4c633b386aa4d9
DanySquirrel <danys2809@gmail.com>
2019-05-25 13:07:43 -0500

    prueba-rama

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-import logo from "../assets/descarga.png";
+// import logo from "../assets/descarga.png";
 import "../css/style.css";
 
 
